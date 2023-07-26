7648e1530f7eacd606f04dade4cf23d83a0e7b86
chauminhnguyen <minhchauss123456@gmail.com>
2023-07-26 15:10:28 +0700

    add_GES

--- es.py
+++ es.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 class GES:
-    def __init__(self, q, sigma, mu):
+    def __init__(self, q, sigma, mu, k):
         '''
         q: number of samples
         sigma: noise scale
@@ -13,9 +13,63 @@ class GES:
         self.q = q
         self.sigma = sigma
         self.mu = mu
-        # self.k = k # subspace dimensions
+        self.k = k # subspace dimensions
         self.alpha = 1
+        self.total_samples = 0
     
+    def run(self, inputs, loss_fn):
+        if self.total_samples >= self.k:
+            self.alpha = 0.5
+        batch_size = inputs.size()[0]
+        channel = inputs.size()[1]
+        h = inputs.size()[2]
+        w = inputs.size()[3]
+        d = channel * h * w
+
+        a = self.sigma * np.sqrt(self.alpha / d)
+        c = self.sigma * np.sqrt((1 - self.alpha) / self.k)
+
+        if self.alpha > 0.5:
+            u_flat = a * torch.rand(1, self.q, d)
+            # self.alpha = 0.5
+        else:
+            u_flat = a * torch.rand(1, self.q, d).cuda() + c * torch.rand(1, self.k).cuda() @ self.U.T
+        
+        u_flat = u_flat.view(batch_size * self.q, d).cuda()
+        u = u_flat.view(-1, channel, h, w)
+
+        with torch.no_grad():
+            mu = torch.tensor(self.mu).cuda()
+
+            # Repeat q times
+            input_q = inputs.repeat((1, self.q, 1, 1)).view(-1, channel, h, w) # batch_size * q, channel, h, w
+            input_q_plus = input_q + mu * u
+            input_q_minus = input_q - mu * u
+
+            loss_tmp_plus = loss_fn(input_q_plus)
+            loss_tmp_minus = loss_fn(input_q_minus)
+
+            loss_diff = torch.tensor(loss_tmp_plus - loss_tmp_minus)
+            
+            grad_est = u_flat * loss_diff.reshape(batch_size * self.q, 1).expand_as(u_flat) / (2 * mu)
+            grad_est = grad_est.view(batch_size, self.q, d).sum(1, keepdim=True).view(batch_size,d) / (2 * self.q * self.sigma ** 2)
+            self.surg_grads.pop(0) if self.surg_grads else self.surg_grads
+            self.surg_grads.append(grad_est)
+
+        surg_grads_tensor = torch.cat(self.surg_grads, dim=0)
+        self.U, _ = torch.linalg.qr(surg_grads_tensor.T)
+
+        recon_flat = torch.flatten(inputs, start_dim=1).cuda()
+        grad_est_no_grad = grad_est.detach()
+        self.total_samples += 2*batch_size
+
+        return grad_est_no_grad, recon_flat
+
+
+class SGES:
+    def __init__(self) -> None:
+        pass
+
     def run(self, inputs, loss_fn):
         batch_size = inputs.size()[0]
         channel = inputs.size()[1]
